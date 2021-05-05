100f57509725882b830eb06edd00129941141e2f
An Nguyen <an.nguyentruong99@gmail.com>
2021-05-05 13:20:40 +0700

    Created collection config files

---
+++ collections/Projects.ts
@@ -0,0 +1,31 @@
+import { CollectionConfig } from 'payload/types';
+import meta from '../fields/meta';
+import slug from '../fields/slug';
+
+const Project: CollectionConfig = {
+  slug: 'projects',
+  fields: [
+    {
+      type: 'text',
+      name: 'projectName',
+      label: 'Project Name',
+      required: true,
+    },
+    {
+      type: 'textarea',
+      name: 'description',
+      label: 'Description',
+      required: true,
+    },
+    {
+      type: 'text',
+      name: 'technologiesUsed',
+      label: 'Technologies Used',
+      required: true,
+    },
+    slug,
+    meta,
+  ],
+};
+
+export default Project;
--- payload.config.ts
+++ payload.config.ts
@@ -2,13 +2,13 @@ import { buildConfig } from 'payload/config';
 import dotenv from 'dotenv';
 import Page from './collections/Page';
 import Media from './collections/Media';
+import FormSubmission from './collections/FormSubmissions';
+import Project from './collections/Projects';
+import BlogPosts from './collections/BlogPosts';
 
 dotenv.config();
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
-  collections: [
-    Page,
-    Media,
-  ],
+  collections: [Page, Media, FormSubmission, Project, BlogPosts],
 });
